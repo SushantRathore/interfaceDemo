6c4b5d5d229be6b1d71f65d45eb668db0cfc84c9
Sushant-Singh-Rathore <ssr7705@gmail.com>
2020-05-16 17:14:03 +0530

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+      // creating instance of interface using lambda expression
         Pc Acer = ()->
                 System.out.println("Ms Word Working.....");
 
+// calling method of interface
         Acer.MsWord();
     }
 
